c76a77d3ce3abacef3b1a2bdea058b300c20a2f9
Tc_traveler <3249456659@qq.com>
2024-06-23 17:03:32 +0800

    Test

--- src/main/java/Tc_traveler/thaumcraftrecipes/items/foci/ItemTRFocus.java
+++ src/main/java/Tc_traveler/thaumcraftrecipes/items/foci/ItemTRFocus.java
@@ -4,7 +4,6 @@ import Tc_traveler.thaumcraftrecipes.creativetab.CreativeTabsLoader;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
 import thaumcraft.api.wands.ItemFocusBasic;
 
 public class ItemTRFocus extends ItemFocusBasic {
